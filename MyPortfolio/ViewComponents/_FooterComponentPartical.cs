22191cba6a71c8299e1b9b0a4ea88b83b3f2472a
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-01-15 01:58:29 +0300

    MyPortfolio

---
+++ MyPortfolio/ViewComponents/_FooterComponentPartical.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _FooterComponentPartical : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
